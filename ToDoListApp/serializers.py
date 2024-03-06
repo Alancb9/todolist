94fcf9990bbfe1ef9eb07890bf7fe8f1e9b239ac
Kevin <kevin.carriel@uees.edu.ec>
2024-03-06 16:30:05 -0500

    commit final

--- ToDoListApp/serializers.py
+++ ToDoListApp/serializers.py
@@ -5,4 +5,4 @@ from .models import ToDo
 class TodoSerializador(serializers.ModelSerializer):
     class Meta:
         model = ToDo
-        fields = ["id", "tarea", "completada"]
+        fields = ["id", "tareas", "completadas"]
